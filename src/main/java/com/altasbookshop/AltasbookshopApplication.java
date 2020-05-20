8f5b8802c59a260257012efe50e0ab43e3e89eb9
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-05-19 22:16:31 -0500

    Intial commit

---
+++ src/main/java/com/altasbookshop/AltasbookshopApplication.java
@@ -0,0 +1,13 @@
+package com.altasbookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltasbookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltasbookshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altasbookshop/AltasbookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altasbookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltasbookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
