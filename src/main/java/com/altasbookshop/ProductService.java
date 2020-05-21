fdc483d48e20bc98b97c47718e82d05de5f3335c
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-05-20 19:41:44 -0500

    added Product Service class and placed all the crud operations

---
+++ src/main/java/com/altasbookshop/ProductService.java
@@ -0,0 +1,52 @@
+package com.altasbookshop;
+
+// Annotate this class with Spring Boot Annotation class
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    // get the ProductRepository and wire it here
+    @Autowired
+    private ProductRepository repo;
+
+    //Here is where you will put all the crud logic
+
+    //1. Read () method -  Get all the products from the database - a list of all products usin list<> method
+
+    public List<Product> listAll(){
+        return  repo.findAll();
+    }
+
+    //2. Save () all products using save method
+
+    public void  save(Product product){
+         repo.save(product);
+    }
+
+
+    //3. Get Product by ID  or Edit() method
+    public Product get(Long id){
+        return repo.findById(id).get(); // in order to get the actual id use get() method
+    }
+
+
+
+    //4 . Delete () Method
+
+public void delete(Long id){
+    repo.deleteById(id);
+}
+
+
+
+
+
+
+
+
+
+}
