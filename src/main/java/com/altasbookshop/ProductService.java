4653e5be8e245e4d888acf5f60c0d31e4b9bf5eb
About Me <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2020-08-23 02:34:34 -0500

    Update ProductService.java

--- src/main/java/com/altasbookshop/ProductService.java
+++ src/main/java/com/altasbookshop/ProductService.java
@@ -16,22 +16,25 @@ public class ProductService {
     //Here is where you will put all the crud logic
     //1. Read () method -  Get all the products from the database - a list of all products usin list<> method
 
+    
+    
+    //Get all the Product lists
     public List<Product> listAll(){
         return  repo.findAll();
     }
 
-    //2. Save () all products using save method
+    //2. Save () all PRODUCTS into the database
     public void  save(Product product){
          repo.save(product);
     }
 
 
-    //3. Get Product by ID  or Edit() method
+    //3. Get PRODUCT by ID  or Find Product by its id
     public Product get(Long id){
         return repo.findById(id).get(); // in order to get the actual id use get() method
     }
 
-    //4 . Delete () Method
+    //4. DELETE single Product using by its id
     // delete product by its id
 public void delete(Long id){
     repo.deleteById(id);
